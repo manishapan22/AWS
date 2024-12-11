3e6611e39deccad0030a3bbf51e17a77b5cc1ff1
manishapan22 <panmanisha12345@gmail.com>
2024-12-11 23:14:59 +0530

    Update delete_snapshots.sh

--- delete_snapshots.sh
+++ delete_snapshots.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Set AWS region
+#!Set AWS region
 REGION="eu-north-1"
 
-# Get snapshots older than 7 days and delete them
+#!Get snapshots older than 7 days and delete them
 DATE_THRESHOLD=$(date -d "-1 days" +%Y-%m-%d)
 
-# List and delete snapshots older than the threshold
+#!List and delete snapshots older than the threshold
 aws ec2 describe-snapshots --region $REGION --query "Snapshots[?StartTime<'$DATE_THRESHOLD'].SnapshotId" --output text | \
 while read snapshot_id; do
     if [[ -n "$snapshot_id" ]]; then
